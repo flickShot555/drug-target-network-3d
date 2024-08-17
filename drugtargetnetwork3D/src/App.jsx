6daaa545240363eb1fe924804e8207469fdafb81
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-08-17 16:39:44 +0500

    udpatet the app.js

--- drugtargetnetwork3D/src/App.jsx
+++ drugtargetnetwork3D/src/App.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import { Layout } from 'antd';
 import Navbar from './components/Navbar.jsx'; // Ensure correct path
-
 import './App.css'; // Import the CSS file
-
-const { Header } = Layout;
-
-// Sample Data for Dropdowns
-const dropdownOptions = [
-  { value: 'apple', label: 'Apple' },
-  { value: 'banana', label: 'Banana' },
-  { value: 'cherry', label: 'Cherry' },
-];
-
-const handleChange = (value) => {
-  console.log('Selected:', value);
-};
-
 const App = () => {
   return (
     <Layout>
       <Navbar />
-
     </Layout>
   );
 };
-
 export default App;

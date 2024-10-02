333ddc76c4db1ed9a4a7b95f88d56d03d6e3af41
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-10-02 11:59:19 +0500

    trying to solve the error there

--- drugtargetnetwork3D/src/components/SinglePIC50.jsx
+++ drugtargetnetwork3D/src/components/SinglePIC50.jsx
@@ -1,33 +1,44 @@
-import { Select, Space } from 'antd';
-import { useDispatch } from 'react-redux';
-import { updateSelection } from './../app/features/data/dataSlice';
+// import { Select, Space } from 'antd';
+// import { useDispatch } from 'react-redux';
+// import { updateSelection } from './../app/features/data/dataSlice';
 
-const PIC50 = [
-  { value: '4', label: '4-9' },
-  { value: '5', label: '5-9' },
-  { value: '6', label: '6-9' },
-  { value: '7', label: '7-9' },
-  { value: '8', label: '8-9' },
-];
+// const PIC50 = [
+//   { value: '4', label: '4-9' },
+//   { value: '5', label: '5-9' },
+//   { value: '6', label: '6-9' },
+//   { value: '7', label: '7-9' },
+//   { value: '8', label: '8-9' },
+// ];
+
+// const SinglePIC50 = () => {
+//   const dispatch = useDispatch();
+
+//   const handleChange = (value) => {
+//     dispatch(updateSelection({ name: 'selectedpic50', value }));
+//   };
+
+//   return (
+//     <Space wrap>
+//       <Select
+//         style={{ width: 120 }}
+//         allowClear
+//         options={PIC50}
+//         placeholder="PIC50"
+//         onChange={handleChange} // Use onChange instead of handleChange
+//       />
+//     </Space>
+//   );
+// };
+
+// export default SinglePIC50;
+import React from 'react'
 
 const SinglePIC50 = () => {
-  const dispatch = useDispatch();
-
-  const handleChange = (value) => {
-    dispatch(updateSelection({ name: 'selectedpic50', value }));
-  };
-
   return (
-    <Space wrap>
-      <Select
-        style={{ width: 120 }}
-        allowClear
-        options={PIC50}
-        placeholder="PIC50"
-        onChange={handleChange} // Use onChange instead of handleChange
-      />
-    </Space>
-  );
-};
+    <div>
+      hi
+    </div>
+  )
+}
 
-export default SinglePIC50;
+export default SinglePIC50
